a14feed121cb7c1909712cf92d6c85a5e01af6cd
unknown <karanjitrishav4@gmail.com>
2020-09-18 20:32:24 +0545

    Updated AddProduct.py

--- AddProduct.py
+++ AddProduct.py
@@ -1,5 +1,7 @@
 from PyQt5 import QtWidgets, uic
 import sys
+from bs4 import BeautifulSoup
+import requests
 
 class AddProductUI(QtWidgets.QDialog):
     def __init__(self):
@@ -8,5 +10,46 @@ class AddProductUI(QtWidgets.QDialog):
 
     def initUI(self): 
         uic.loadUi('./UI/AddProduct.ui', self) 
-               
+        self.PName = self.findChild(QtWidgets.QLineEdit, 'PName')
+        self.PLink = self.findChild(QtWidgets.QLineEdit, 'PLink')
+        self.Save = self.findChild(QtWidgets.QPushButton, 'Save')
+        self.Price= self.findChild(QtWidgets.QLabel,'Price')
+        self.CheckPrice = self.findChild(QtWidgets.QPushButton, 'CheckPrice')
+
+        self.CheckPrice.clicked.connect(self.DispPrice)
+        self.Save.clicked.connect(self.SaveProduct)
         self.show()
+
+    def DispPrice(self):
+        ProName = self.PName.text()
+        ProLink = self.PLink.text()
+        if bool(ProLink):
+            try:
+                source0 = requests.get(ProLink,headers={'user-agent':'Chrome'})
+                source = source0.text
+                data = BeautifulSoup(source,'lxml')
+                if 'amazon' in ProLink:
+                    if(data.find('span',class_="a-size-medium a-color-price priceBlockDealPriceString")):
+                        price0 = data.find('span',class_="a-size-medium a-color-price priceBlockDealPriceString").text
+                    else:
+                        price0 = data.find('span',class_="a-size-medium a-color-price priceBlockBuyingPriceString").text
+                    self.Price.setText(price0)
+
+            except:
+                self.Price.setText("Invalid URL")
+                
+    
+    def SaveProduct(self):
+        ProName = self.PName.text()
+        ProLink = self.PLink.text()
+        if bool(ProLink):
+            source0 = requests.get(ProLink,headers={'user-agent':'Chrome'})
+            source = source0.text
+            data = BeautifulSoup(source,'lxml')
+            if 'amazon' in ProLink:
+                if(data.find('span',class_="a-size-medium a-color-price priceBlockDealPriceString")):
+                    price0 = data.find('span',class_="a-size-medium a-color-price priceBlockDealPriceString").text
+                else:
+                    price0 = data.find('span',class_="a-size-medium a-color-price priceBlockBuyingPriceString").text
+                print(price0)
+
