513f5b0e0ac1a5b8465a1dd2cb145e0f59845253
unknown <karanjitrishav4@gmail.com>
2020-09-19 15:13:46 +0545

    Completed ProductList.py

--- ProductList.py
+++ ProductList.py
@@ -18,10 +18,19 @@ class ProductListUI(QtWidgets.QDialog):
         self.Price = self.findChild(QtWidgets.QLabel, 'Price')
         self.URLtext = self.findChild(QtWidgets.QPlainTextEdit, 'URLtext')
 
-        self.CheckPrice.clicked.connect(self.DispPrice)
+        self.ShowButton.clicked.connect(self.DispPrice)
 
-        cursor.execute('SHOW TABLE')
+        cursor.execute('SHOW TABLES')
         all_product = cursor.fetchall()
 
-        print(all_product)
+        for i in all_product:
+            self.PName.addItem(i[0])
         self.show()
+
+    def DispPrice(self):
+        ProName = str(self.PName.currentText())
+        cursor.execute("SELECT * FROM {ProName}".format(ProName=ProName))
+        items = cursor.fetchone()
+
+        self.Price.setText(str(items[1]))
+        self.URLtext.insertPlainText(items[0])
