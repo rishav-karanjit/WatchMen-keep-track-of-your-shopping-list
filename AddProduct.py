5ff4d81159dee145964f12acc3dfb510f8b2f375
unknown <karanjitrishav4@gmail.com>
2020-09-19 11:56:01 +0545

    Added MySQL connection

--- AddProduct.py
+++ AddProduct.py
@@ -2,7 +2,10 @@ from PyQt5 import QtWidgets, uic
 import sys
 from bs4 import BeautifulSoup
 import requests
+import mysql.connector
 
+db = mysql.connector.connect(host='localhost',user='root',password='root',database='WatchMen')
+cursor = db.cursor()
 class AddProductUI(QtWidgets.QDialog):
     def __init__(self):
         super(AddProductUI, self).__init__()
@@ -24,27 +27,34 @@ class AddProductUI(QtWidgets.QDialog):
         ProName = self.PName.text()
         ProLink = self.PLink.text()
         if bool(ProLink):
-            try:
-                source0 = requests.get(ProLink,headers={'user-agent':'Chrome'})
-                source = source0.text
-                data = BeautifulSoup(source,'lxml')
-                if 'amazon' in ProLink:
-                    if(data.find('span',class_="a-size-medium a-color-price priceBlockDealPriceString")):
-                        price0 = data.find('span',class_="a-size-medium a-color-price priceBlockDealPriceString").text
-                    else:
-                        price0 = data.find('span',class_="a-size-medium a-color-price priceBlockBuyingPriceString").text
-                    self.Price.setText(price0)
+            # try:
+            source0 = requests.get(ProLink,headers={'user-agent':'Chrome'})
+            source = source0.text
+            data = BeautifulSoup(source,'lxml')
+            if 'amazon' in ProLink:
+                if(data.find('span',class_="a-size-medium a-color-price priceBlockDealPriceString")):
+                    price0 = data.find('span',class_="a-size-medium a-color-price priceBlockDealPriceString").text
+                else:
+                    price0 = data.find('span',class_="a-size-medium a-color-price priceBlockBuyingPriceString").text
+                self.Price.setText(str(price0))
 
-                if 'flipkart.com' in ProLink:
-                    price0 = data.find('div',class_="_1vC4OE _3qQ9m1").text
-                    self.Price.setText(price0)
-            except:
-                self.Price.setText("Invalid URL")
+            if 'flipkart.com' in ProLink:
+                price0 = data.find('div',class_="_1vC4OE _3qQ9m1").text
+                self.Price.setText(str(price0))
+            # except:
+            #     self.Price.setText("Invalid URL")
                 
     
     def SaveProduct(self):
         ProName = self.PName.text()
         ProLink = self.PLink.text()
+        Link = f'"{ProLink}"'
+        print(Link)
+        ProName = ProName.replace(" ", "")
+        whitelist = set('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
+        ProName = ''.join(filter(whitelist.__contains__, ProName))
+        print(ProName)
+
         if bool(ProLink):
             try:
                 source0 = requests.get(ProLink,headers={'user-agent':'Chrome'})
@@ -55,12 +65,26 @@ class AddProductUI(QtWidgets.QDialog):
                         price0 = data.find('span',class_="a-size-medium a-color-price priceBlockDealPriceString").text
                     else:
                         price0 = data.find('span',class_="a-size-medium a-color-price priceBlockBuyingPriceString").text
+                    price0 = ''.join(i for i in price0 if i.isdigit() or i == '.')
+                    price0,a,a = price0.partition(".")
+                    price0 = int(price0)
                     print(price0)
-
+                    
+                    cursor.execute('CREATE TABLE {tname} (Product_Link varchar(1000),Price int)'.format(tname=ProName))
+                    cursor.execute('INSERT INTO {tname} VALUES({PLink},{price})'.format(tname=ProName,PLink=Link,price=price0)) 
+                    db.commit()
+                
                 if 'flipkart.com' in ProLink:
                     price0 = data.find('div',class_="_1vC4OE _3qQ9m1").text
                     print(price0)
+                    price0 = ''.join(i for i in price0 if i.isdigit() or i == '.')
+                    price0,a,a = price0.partition(".")
+                    price0 = int(price0)
+                    print(price0)
+                    
+                    cursor.execute('CREATE TABLE {tname} (Product_Link varchar(1000),Price int)'.format(tname=ProName))
+                    cursor.execute('INSERT INTO {tname} VALUES({PLink},{price})'.format(tname=ProName,PLink=Link,price=price0)) 
+                    db.commit()
                 
             except:
                 self.Price.setText("Invalid URL")
-
