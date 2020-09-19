afebe4b610d97ab5a50610dd0423cd71043f7f56
unknown <karanjitrishav4@gmail.com>
2020-09-19 15:36:22 +0545

    Delete Product Completed

--- DeleteProduct.py
+++ DeleteProduct.py
@@ -1,6 +1,12 @@
 from PyQt5 import QtWidgets, uic
 import sys
 
+import MainUI
+import mysql.connector
+
+db = mysql.connector.connect(host='localhost',user='root',password='root',database='WatchMen')
+cursor = db.cursor()
+
 class DeleteProductUI(QtWidgets.QDialog):
     def __init__(self):
         super(DeleteProductUI, self).__init__()
@@ -8,5 +14,28 @@ class DeleteProductUI(QtWidgets.QDialog):
 
     def initUI(self): 
         uic.loadUi('./UI/DeleteProduct.ui', self) 
-               
+        
+        self.PName = self.findChild(QtWidgets.QComboBox, 'PName')
+        self.DelProduct_B = self.findChild(QtWidgets.QPushButton, 'DelProduct_B')
+        self.Cancel = self.findChild(QtWidgets.QPushButton, 'Cancel')
+
+        self.Cancel.clicked.connect(self.GoBack)
+        self.DelProduct_B.clicked.connect(self.DelP)
+
+        cursor.execute('SHOW TABLES')
+        all_product = cursor.fetchall()
+
+        for i in all_product:
+            self.PName.addItem(i[0])
+        
         self.show()
+    
+    def DelP(self):
+        ProName = str(self.PName.currentText())
+        cursor.execute("DROP TABLE {ProName}".format(ProName=ProName))
+        self.dialog = MainUI.MainUI()
+        self.close()
+
+    def GoBack(self):
+        self.dialog = MainUI.MainUI()
+        self.close()
