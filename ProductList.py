e8ffd826a34919308e1fde386c200eb3f0c5d96a
unknown <karanjitrishav4@gmail.com>
2020-09-19 14:08:37 +0545

    Updated Product List UI

--- ProductList.py
+++ ProductList.py
@@ -8,5 +8,11 @@ class ProductListUI(QtWidgets.QDialog):
 
     def initUI(self): 
         uic.loadUi('./UI/ProductList.ui', self) 
-               
+        
+        self.PName = self.findChild(QtWidgets.QComboBox, 'PName')
+        self.ShowButton = self.findChild(QtWidgets.QPushButton, 'ShowButton')
+        self.Price = self.findChild(QtWidgets.QLabel, 'Price')
+        self.URLtext = self.findChild(QtWidgets.QPlainTextEdit, 'URLtext')
+
+        self.CheckPrice.clicked.connect(self.DispPrice)
         self.show()
